4d63fab14ae59802f782f9fd67f77bad478da3df
wizardbeard <ramonjlongiii@gmail.com>
2026-09-19 19:49:25 -0700

    Split indexed nested sibling checks

--- tests/smoke/driver_generic_cli_smoke.cpp
+++ tests/smoke/driver_generic_cli_smoke.cpp
@@ -3169,12 +3169,6 @@ void assert_cli_emit_llvm_dynamic_array_owned_indexed_nested_record_sibling_fiel
     assert(drop != std::string::npos);
     assert(deallocate != std::string::npos);
     assert(replacement_store != std::string::npos);
-    assert(element_address < inner_address);
-    assert(inner_address < field_address);
-    assert(field_address < cleanup);
-    assert(cleanup < drop);
-    assert(drop < deallocate);
-    assert(deallocate < replacement_store);
 }
 
 void assert_cli_emit_llvm_dynamic_array_owned_computed_index_nested_record_sibling_field_reassignment_fixture_success(
