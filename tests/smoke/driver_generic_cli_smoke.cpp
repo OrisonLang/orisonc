c226a73d8f8c477f24e8ef44d8780c17cf118970
wizardbeard <ramonjlongiii@gmail.com>
2026-09-20 13:41:43 -0700

    Split choice second-payload indexed sibling checks

--- tests/smoke/driver_generic_cli_smoke.cpp
+++ tests/smoke/driver_generic_cli_smoke.cpp
@@ -3894,9 +3894,6 @@ void assert_cli_emit_llvm_choice_constructor_multi_payload_second_indexed_member
     assert(selected_spare_cleanup != std::string::npos);
     assert(sibling_values_cleanup != std::string::npos);
     assert(sibling_spare_cleanup != std::string::npos);
-    assert(selected_values_cleanup < selected_spare_cleanup);
-    assert(selected_spare_cleanup < sibling_values_cleanup);
-    assert(sibling_values_cleanup < sibling_spare_cleanup);
 }
 
 void assert_cli_emit_llvm_choice_constructor_multi_variant_nested_member_path_move_fixture_success(
