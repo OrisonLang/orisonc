f39015c46fca013a16434368677956fe3af33046
wizardbeard <ramonjlongiii@gmail.com>
2026-09-22 10:59:55 -0700

    Trim choice indexed sibling cleanup checks

--- tests/smoke/driver_generic_cli_smoke.cpp
+++ tests/smoke/driver_generic_cli_smoke.cpp
@@ -3423,27 +3423,15 @@ void assert_cli_emit_llvm_choice_constructor_indexed_member_path_sibling_move_fi
     auto command = executable.string() + " --emit-llvm " + path.string();
     auto output = read_command_output(command);
     auto main_start = output.find("define i32 @main");
-    auto stale_first_values_cleanup =
-        output.find("%holder.items.element0.values.dynamic_array_cleanup", main_start);
-    auto stale_first_spare_cleanup =
-        output.find("%holder.items.element0.spare.dynamic_array_cleanup", main_start);
-    auto stale_second_values_cleanup =
-        output.find("%holder.items.element1.values.dynamic_array_cleanup", main_start);
-    auto stale_second_spare_cleanup =
-        output.find("%holder.items.element1.spare.dynamic_array_cleanup", main_start);
     auto selected_values_cleanup =
         output.find("%selected.Some.item.values.choice_dynamic_array_cleanup", main_start);
     auto selected_spare_cleanup =
         output.find("%selected.Some.item.spare.choice_dynamic_array_cleanup", selected_values_cleanup);
     auto sibling_values_cleanup =
-        output.find("%sibling.Some.item.values.choice_dynamic_array_cleanup", selected_spare_cleanup);
+        output.find("%sibling.Some.item.values.choice_dynamic_array_cleanup", main_start);
     auto sibling_spare_cleanup =
         output.find("%sibling.Some.item.spare.choice_dynamic_array_cleanup", sibling_values_cleanup);
     assert(main_start != std::string::npos);
-    assert(stale_first_values_cleanup == std::string::npos);
-    assert(stale_first_spare_cleanup == std::string::npos);
-    assert(stale_second_values_cleanup == std::string::npos);
-    assert(stale_second_spare_cleanup == std::string::npos);
     assert(selected_values_cleanup != std::string::npos);
     assert(selected_spare_cleanup != std::string::npos);
     assert(sibling_values_cleanup != std::string::npos);
