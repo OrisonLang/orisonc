93b5cb3406a7c7215242099b2dd30176cfa0b59a
wizardbeard <ramonjlongiii@gmail.com>
2026-09-24 20:08:07 -0700

    Trim choice constructor move checks

--- tests/smoke/driver_generic_cli_smoke.cpp
+++ tests/smoke/driver_generic_cli_smoke.cpp
@@ -3336,16 +3336,8 @@ void assert_cli_emit_llvm_choice_constructor_member_path_move_fixture_success(
     auto output = read_command_output(command);
     auto main_start = output.find("define i32 @main");
     auto selected_cleanup = output.find("%selected.Some.values.choice_dynamic_array_cleanup", main_start);
-    auto selected_drop = output.find(
-        "call void @__orison_owned_cleanup.Payload(ptr %selected.Some.values.choice_dynamic_array_cleanup"
-    );
-    auto selected_deallocate = output.find(
-        "call void @__orison_dynamic_array_deallocate(ptr %selected.Some.values.choice_dynamic_array_cleanup"
-    );
     assert(main_start != std::string::npos);
     assert(selected_cleanup != std::string::npos);
-    assert(selected_drop != std::string::npos);
-    assert(selected_deallocate != std::string::npos);
 }
 
 void assert_cli_emit_llvm_choice_constructor_nested_member_path_move_fixture_success(
