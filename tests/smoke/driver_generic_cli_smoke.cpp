f5a9bb73e15cbd85fa292d9339c18d879c67af4c
wizardbeard <ramonjlongiii@gmail.com>
2026-09-25 02:12:01 -0700

    Trim constructor nested cleanup checks

--- tests/smoke/driver_generic_cli_smoke.cpp
+++ tests/smoke/driver_generic_cli_smoke.cpp
@@ -3568,17 +3568,9 @@ void assert_cli_emit_llvm_dynamic_array_owned_constructor_nested_member_path_mov
     auto output = read_command_output(command);
     auto main_start = output.find("define i32 @main");
     auto replacement_cleanup = output.find("%outer.items.element0.values.dynamic_array_reassign_cleanup", main_start);
-    auto replacement_drop = output.find(
-        "call void @__orison_owned_cleanup.Payload(ptr %outer.items.element0.values.dynamic_array_reassign_cleanup"
-    );
-    auto replacement_deallocate = output.find(
-        "call void @__orison_dynamic_array_deallocate(ptr %outer.items.element0.values.dynamic_array_reassign_cleanup"
-    );
     auto final_outer_drop = output.find("call void @__orison_owned_cleanup.Outer(ptr %outer.addr)");
     assert(main_start != std::string::npos);
     assert(replacement_cleanup != std::string::npos);
-    assert(replacement_drop != std::string::npos);
-    assert(replacement_deallocate != std::string::npos);
     assert(final_outer_drop != std::string::npos);
 }
 
