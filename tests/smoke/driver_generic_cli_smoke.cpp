ac015b07f8ae78312e4c53a75a01bfa6dab3a5cf
wizardbeard <ramonjlongiii@gmail.com>
2026-09-24 21:00:50 -0700

    Trim choice constructor sibling checks

--- tests/smoke/driver_generic_cli_smoke.cpp
+++ tests/smoke/driver_generic_cli_smoke.cpp
@@ -3375,17 +3375,11 @@ void assert_cli_emit_llvm_choice_constructor_indexed_member_path_sibling_move_fi
     auto main_start = output.find("define i32 @main");
     auto selected_values_cleanup =
         output.find("%selected.Some.item.values.choice_dynamic_array_cleanup", main_start);
-    auto selected_spare_cleanup =
-        output.find("%selected.Some.item.spare.choice_dynamic_array_cleanup");
     auto sibling_values_cleanup =
         output.find("%sibling.Some.item.values.choice_dynamic_array_cleanup", main_start);
-    auto sibling_spare_cleanup =
-        output.find("%sibling.Some.item.spare.choice_dynamic_array_cleanup");
     assert(main_start != std::string::npos);
     assert(selected_values_cleanup != std::string::npos);
-    assert(selected_spare_cleanup != std::string::npos);
     assert(sibling_values_cleanup != std::string::npos);
-    assert(sibling_spare_cleanup != std::string::npos);
 }
 
 void assert_cli_emit_llvm_choice_constructor_multi_payload_nested_member_path_move_fixture_success(
