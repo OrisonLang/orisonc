3150c9fb246a2d9acb5d75f0edd7421494d3dfa1
wizardbeard <ramonjlongiii@gmail.com>
2026-09-26 00:58:06 -0700

    Trim nested dynamic index record field checks

--- tests/smoke/driver_generic_cli_smoke.cpp
+++ tests/smoke/driver_generic_cli_smoke.cpp
@@ -3015,12 +3015,6 @@ void assert_cli_emit_llvm_dynamic_array_owned_nested_dynamic_index_record_field_
     auto nested_element = output.find(".element.addr = getelementptr %record.Item", nested_bounds);
     auto field_address = output.find("getelementptr %record.Item, ptr %groups.element.items.dynamic_array_index", nested_element);
     auto cleanup = output.find("%groups.element.items.element.values.dynamic_array_reassign_cleanup");
-    auto drop = output.find(
-        "call void @__orison_owned_cleanup.Payload(ptr %groups.element.items.element.values.dynamic_array_reassign_cleanup"
-    );
-    auto deallocate = output.find(
-        "call void @__orison_dynamic_array_deallocate(ptr %groups.element.items.element.values.dynamic_array_reassign_cleanup"
-    );
     auto replacement_store = output.find("store { ptr, i64, i64 } %tmp");
     assert(outer_descriptor != std::string::npos);
     assert(outer_bounds != std::string::npos);
@@ -3030,8 +3024,6 @@ void assert_cli_emit_llvm_dynamic_array_owned_nested_dynamic_index_record_field_
     assert(nested_element != std::string::npos);
     assert(field_address != std::string::npos);
     assert(cleanup != std::string::npos);
-    assert(drop != std::string::npos);
-    assert(deallocate != std::string::npos);
     assert(replacement_store != std::string::npos);
 }
 
